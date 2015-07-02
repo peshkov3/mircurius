6644e0b307c446aa7a0dbe1d631030ea913be10b
peshkov3 <peshkov-maximum@yandex.ru>
2015-07-02 22:39:58 +0600

    copmmit

--- database/migrations/2015_06_28_125708_create_products_table.php
+++ database/migrations/2015_06_28_125708_create_products_table.php
@@ -15,7 +15,18 @@ class CreateProductsTable extends Migration {
 		Schema::create('products', function(Blueprint $table)
 		{
 			$table->increments('id');
+            $table->bigInteger('product_id');c
+             $table->char('sid',60);c
+              $table->bigInteger('type_id');c
+               $table->bigInteger('has_discount');c
+               
+               
+                $table->boolean('is_disabled');c
+                 $table->string('price');c
+                  $table->bigInteger('product_id');c 
+                  $table->bigInteger('product_id');c
 			$table->timestamps();
+            
 		});
 	}
 
